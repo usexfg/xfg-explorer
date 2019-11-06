e03bad56b47b07e4a64aefb8676db4df7a444c07
ColinRitman <54386876+ColinRitman@users.noreply.github.com>
2019-11-06 11:49:28 -0500

    Update crypto_utils.js
    XFG Fandom GOLD changes

--- js/crypto_utils.js
+++ js/crypto_utils.js
@@ -1625,9 +1625,9 @@ return{_strlen:lb,_ge_mul8:Va,_keccak:db,_ge_scalarmult:Ta,_ge_fromfe_frombytes_
 var salt = '4721283fee2db41e1f2fc50ea9f6d783'; // IMPORTANT: never change it, otherwise it will be impossible to restore previously generated wallets!
 var config = {
     coinUnitPlaces: 7,
-    coinSymbol: 'DRGL',
-    coinName: 'Dragonglass',
-    coinUriPrefix: 'dragonglass:',
+    coinSymbol: 'XFG',
+    coinName: 'Fandom GOLD',
+    coinUriPrefix: 'fandomgold:',
     addressPrefix: 679385
 };
 var cnUtil = (function(initConfig) {
