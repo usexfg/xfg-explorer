d385b9457fcd46c78570261739b85d1be761c146
ColinRitman <54386876+ColinRitman@users.noreply.github.com>
2019-11-16 17:59:48 -0500

    Update config.js
    8min blocks
    ticker XFG

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 var api = 'http://zirtysperzys.info:18180';
 var apiList = [];
-var blockTargetInterval = 81;
+var blockTargetInterval = 480;
 var coinUnits = 10000000;
-var symbol = 'DRGL';
-var refreshDelay = 8100;
+var symbol = 'XFG';
+var refreshDelay = 18000;
 var networkStat = {
-    "DRGL": [
-	["DRGL.online", "https://drgl.online/api"],
-	["crypto9coin.cf", "https://crypto9coin.cf:18183"]
-    ]
 };
