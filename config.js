8a91d3af6d3631116b55604968400c60c9840e8e
ÆJT <aejontargaryen@protonmail.com>
2023-03-31 23:23:36 -0400

    api node

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://fango.money:18180';
+var api = 'http://3.16.217.33:18180';
 var poolListUrl = 'https://fangotango.hopto.org/api/pools/data';
 var donationAddress = "";
 var blockTargetInterval = 480;
