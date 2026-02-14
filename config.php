c736192bf7bc997bd0a5331e699e5335628306c2
ÆJT <aejontargaryen@protonmail.com>
2026-02-14 10:53:06 -0500

    Update API endpoint in config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-        'api' => 'http://127.0.0.1:18180',
+        'api' => 'http://127.0.0.1:28280',
         'blockTargetInterval' => 480,
         'coinUnits' => 10000000
 );
