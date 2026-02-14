756731252068b3e79ea3bbdbe68878ffb6bf29a4
ÆJT <aejontargaryen@protonmail.com>
2026-02-14 10:52:33 -0500

    Update API endpoint and symbol in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-var api = 'http://127.0.0.1:18180';
+var api = 'http://127.0.0.1:28280';
 var poolListUrl = '';
 var donationAddress = "";
 var blockTargetInterval = 480;
 var coinUnits = 10000000;
-var symbol = 'XFG';
+var symbol = 'TEST';
 var refreshDelay= 48000;
